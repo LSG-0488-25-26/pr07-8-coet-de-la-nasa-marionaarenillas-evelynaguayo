6e695462760330b9e4b681f19fcdf7a5aa826d2f
LynAguayo <lyn.aguayoc@gmail.com>
2026-01-22 16:56:21 +0100

    Create RetrofitClient

--- app/src/main/java/com/example/consumapi/network/RetrofitClient.kt
+++ app/src/main/java/com/example/consumapi/network/RetrofitClient.kt
@@ -1,4 +1,19 @@
 package com.example.consumapi.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClient {
+    private const val BASE_URL = "https://api.fbi.gov/"
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val apiService: FbiApiService by lazy {
+        retrofit.create(FbiApiService::class.java)
+    }
 }
